02ef454d9c9c0cf113fff51f524f2cda315c56ed
chinedum onyema <onyemachinedum@gmail.com>
2020-09-06 05:18:51 +0100

    refactor(utililies): modify util functions > Rename function modified accepts a string input > fileStore function will return just file ID and a secure URL in JSON > parseForm function modified to accept only one file from a form

--- api/util/renameFile.js
+++ api/util/renameFile.js
@@ -1,10 +1,10 @@
 /**
- *
+ * Returns a string of characters safe for use as file name
  * @param {string} name - name  of the raw file
- * @returns A string of safe character file name
+ *
  */
 
-module.exports = ({name}) => {
+module.exports = (name) => {
   let lName = name.toLowerCase().split('.')[0];
   // keep only desired characters
   let renamedFile = lName.replace(/[^a-zA-Z0-9-_]/g, '');
