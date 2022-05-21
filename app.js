789ebe9f605085bdae54f60340113fc489dfa84f
chinedum onyema <onyemachinedum@gmail.com>
2022-05-21 18:32:28 +0100

    refactor: modify server for various environment

--- app.js
+++ app.js
@@ -1,24 +1,62 @@
+// Environment configuration
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Import necessary packages
 const cors = require("cors");
 const express = require("express");
-const dotenv = require('dotenv');
-const debug = require('debug')('app:server');
-// const fileUpload = require("express-fileupload");
+const debug = require('debug')('api:server');
 const logger = require("morgan");
-// const path = require('path');
+const path = require('path');
+const csrf = require('csurf');
 const helmet = require("helmet");
+const passport = require("passport");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+// Configuration and helper modules
+const passportConfig = require('./config/passport');
+const errorHandler = require("./lib/errorHandler");
+const db = require('./config/sequelize/models');
 
 
-const errorHandler = require("./util/errorHandler");
-
+// Routers
 const api = require("./api");
 const dashboard = require("./dashboard");
 // const web = require("./web");
 
 
-// environment configuration
-dotenv.config();
+// Initalize sequelize with session store
+const sessionStore = new SequelizeStore({
+  db: db.sequelize,
+});
+sessionStore.sync();
+
+
 const app = express();
 const port = process.env.PORT || 4000;
+
+// Configure passport
+passportConfig(passport);
+
+// Session options
+const oneDay = 1000 * 60 * 60 * 24;
+const sessOptions = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {
+    maxAge: oneDay,
+    secure: true,
+  },
+  store: sessionStore,
+  resave: false,
+  saveUninitialized: false,
+};
+
+
+// app.use('/static', express.static('web/src'))
+// app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.static(path.join(__dirname, 'dashboard/src')));
+
 
 // middlewares for production env.
 app.use(helmet());
@@ -31,14 +69,23 @@ app.use(
   })
 );
 
+app.use(session(sessOptions));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// after session
+app.use(csrf());
 
 // sub-apps and routes
-app.use('/v1', api);
+// app.use('/v1', api);
 app.use('/manage', dashboard);
-// app.use('/api/v1', api);
-// app.use('/manage', dashboard);
-// app.use('/', web);
-// app.use('/api/v1', api);
+app.use('/', api);
+
+
+app.get('*', (req, res) => {
+  res.redirect('/');
+});
 
 // app.get('*', (req, res) => {
 //   res.json({ message: 'Welcome to the SEC!!!' });
@@ -49,7 +96,7 @@ app.use(errorHandler);
 
 // start app server
 app.listen(port, () => {
-  debug(`Api running on port ${port}.`);
+  debug(`Api server running on port ${port}.`);
 });
 
 module.exports = app;
